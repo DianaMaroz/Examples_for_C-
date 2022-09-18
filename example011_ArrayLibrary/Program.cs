d7250e9001c5a3dd836a1a128a1f90bcefb42561
Diana\ Moroz <diva14@yandex.by>
2022-09-18 16:58:49 +0300

    how to create array with random and search index of some element

---
+++ example011_ArrayLibrary/Program.cs
@@ -0,0 +1,47 @@
+﻿//заполнение массива случайными числами
+void FillArray(int[] collection) // void - функция без return , в качестве аргумента массив
+{
+    int lenght = collection.Length;
+    int index = 0;
+    while(index < lenght)
+    {
+        collection[index] = new Random().Next(0, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int pos = 0;
+    while(pos < count){
+        Console.WriteLine(col[pos]);
+        pos++;
+    }
+}
+// функция поиска заданного элемента в массиве
+int IndexOf (int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int indexOf = -1; // если элемент отсутвует в списке вернет -1
+    while (index < count) 
+    {
+        if (collection[index] == find) 
+        {
+            indexOf = index;
+            break;
+        }
+        index++;
+    }
+    return indexOf;
+}
+
+
+int[] array = new int[10]; // создание массива с десятью ячейками, по умолчанию заполнено нулями
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+int indexOf = IndexOf(array, 4);
+Console.WriteLine(indexOf);
+
